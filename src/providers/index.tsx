07c16009e98e14b5e75765b7883ceb45d7501456
VLDR <melvernardell@gmail.com>
2025-02-07 12:11:01 +0700

    chore(deps): upgrade next.js to v15

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { QueryProvider } from "@/providers/queryProvider";
 import { ThemeProvider } from "@/providers/themeProvider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return <QueryProvider>{children}</QueryProvider>;
+	}
+
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-			disableTransitionOnChange
-		>
+		<ThemeProvider attribute="class" defaultTheme="light" enableSystem>
 			<QueryProvider>{children}</QueryProvider>
 		</ThemeProvider>
 	);
